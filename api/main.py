6ed9b920f58d52f77122fb42d0b0350722aac880
James Patten <jamespatten@192.168.1.238>
2023-05-12 09:15:06 +0100

    session stuff

--- api/main.py
+++ api/main.py
@@ -73,7 +73,7 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
-app.add_middleware(SessionMiddleware, secret_key=SECRET_KEY, secure=True)
+app.add_middleware(SessionMiddleware, secret_key=SECRET_KEY)
 
 app.include_router(connector_router.router)
 app.include_router(query_router.router)
