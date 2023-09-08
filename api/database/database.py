7407386f7d5f0beeb5977a3a5174765b277db5ca
James Patten <jamespatten1996@gmail.com>
2023-09-08 08:02:35 +0100

    added some sqlalchemy db connection args

--- api/database/database.py
+++ api/database/database.py
@@ -5,7 +5,14 @@ from api.models.user import User
 from api.config import Config
 
 db_uri = Config.DATABASE_URL
-engine = create_engine(db_uri, pool_size=20, max_overflow=10)
+engine = create_engine(
+    db_uri,
+    pool_size=20,
+    max_overflow=10,
+    pool_recycle=300,
+    pool_pre_ping=True,
+    pool_use_lifo=True,
+)
 Base = declarative_base()
 SessionLocal = sessionmaker(bind=engine)
 session = SessionLocal()
